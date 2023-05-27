0d65d0ca9606923dda2764e3ef20e7957577fac9
Марк <chuykov.andrey.2001@gmail.com>
2023-05-27 20:08:23 +0500

    Последний штрих.

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/skyrat/story_participant.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/skyrat/story_participant.tsx
@@ -1,6 +1,6 @@
 import { CheckboxInput, FeatureToggle } from '../../base';
 
-export const story_pref: FeatureToggle = {
+export const story_participant_pref: FeatureToggle = {
   name: 'Be story participant',
   category: 'GAMEPLAY',
   description: 'Toggles whether you wish to participate in stories or not.',
